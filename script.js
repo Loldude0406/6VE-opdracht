91e21e26502de998f662ec66cdb89b46442a1b37
Loldude0406 <chris.vijn0406@gmail.com>
2021-09-09 16:30:43 +0000

    1 bal

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
-var x = 50;
-var y = 50; 
+var Bal1x = 50;
+var Bal1y = 50; 
+var SnelheidX1 = 10;
+var SnelheidY1 = 10;
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
@@ -20,10 +22,26 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
-  background('blue');
+  background('black');
   // stel vulkleur in
-  fill(100, 100, 255);
-
+  fill(255, 255, 255);
+  Bal1x = Bal1x + SnelheidX1;
+  Bal1y = Bal1y + SnelheidY1;
   // teken een cirkel
-  ellipse(x ,y ,80 , 80);
-}
+  ellipse(Bal1x ,Bal1y ,80 , 80);
+
+  
+  if ( Bal1x === 1240){
+    SnelheidX1 = SnelheidX1 * -1;
+  }
+  if ( Bal1x === 40){
+    SnelheidX1 = SnelheidX1 * -1; 
+  }
+  if ( Bal1y === 680){
+    SnelheidY1 = SnelheidY1 * -1;
+  }
+  if ( Bal1y === 40){
+    SnelheidY1 = SnelheidY1 * -1;
+  }
+
+ }
