50459ffb370491fd4e71b72339bf96c0bc84a161
Loldude0406 <chris.vijn0406@gmail.com>
2021-09-13 11:59:03 +0000

    niks werkt

--- bal.js
+++ bal.js
@@ -8,13 +8,22 @@ class Bal {
         this.x = _x;
         this.y = _y;
         speedX = 3;
-        speedY = -2; 
+        speedY = -4; 
     }
-    show() {
-        fill(this.x, this.y, 50, 50);
-    }
+    
     update() {
         this.x = this.x + this.speedX;
         this.y = this.y + this.speedY;
+
+        if(this.x <=0 || this.x >=1280){
+            this.speedX = this.speedX * -1;
+        }
+        if(this.y <=0 || this.x >=720){
+            this.speedY = this.speedY * -1;
+        }
+    }
+    show() {
+        fill(this.x, this.y, 50, 50);
+        ellipse(this.x, this.y, 50, 50);
     }
 }
--- script.js
+++ script.js
@@ -3,10 +3,7 @@
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
-var Bal1x = 50;
-var Bal1y = 50; 
-var SnelheidX1 = 10;
-var SnelheidY1 = 10;
+var bal = new Bal(50, 100);
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
@@ -22,26 +19,8 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
-  background('black');
-  // stel vulkleur in
-  fill(255, 255, 255);
-  Bal1x = Bal1x + SnelheidX1;
-  Bal1y = Bal1y + SnelheidY1;
-  // teken een cirkel
-  ellipse(Bal1x ,Bal1y ,80 , 80);
+  background('blue');
 
-  
-  if ( Bal1x === 1240){
-    SnelheidX1 = SnelheidX1 * -1;
-  }
-  if ( Bal1x === 40){
-    SnelheidX1 = SnelheidX1 * -1; 
-  }
-  if ( Bal1y === 680){
-    SnelheidY1 = SnelheidY1 * -1;
-  }
-  if ( Bal1y === 40){
-    SnelheidY1 = SnelheidY1 * -1;
-  }
-
- }
+  bal.show();
+  bal.update();
+}
